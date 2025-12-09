78257af15b6380567f15d5dc32a65b56550a1f87
root <root@ip-172-31-4-58.ap-northeast-3.compute.internal>
2025-12-09 09:06:05 +0000

    backend-updated

--- FlightReservationApplication/backend.groovy
+++ FlightReservationApplication/backend.groovy
@@ -6,6 +6,50 @@ pipeline{
                git branch: 'main', url: 'https://github.com/Vedant8850/Flight-reservation-vedant.git'
             }
         }
-   }
+
+	stage('Build'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    mvn clean package
+                '''
+            }
+        }
+
+	  stage('QA-Test'){
+            steps{
+                withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-token') {
+                    sh '''
+                        cd FlightReservationApplication
+                        mvn sonar:sonar  -Dsonar.projectKey=flight-reservation
+                    '''
+                }       
+            }
+        }
+
+	stage('Docker-build'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    docker build -t vedant8850/flight-reservation-demo:latest .
+                    docker push vedant8850/flight-reservation-demo:latest
+                    docker rmi vedant8850/flight-reservation-demo:latest
+                '''
+            }
+        }
+
+	     stage('Deploy'){
+            steps{
+                sh '''
+                    cd FlightReservationApplication
+                    kubectl apply -f k8s/
+                '''
+            }
+        }
+    }
 }
+	
+
+
+
 
